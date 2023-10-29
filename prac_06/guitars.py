045d74575c0221c8ac7658bb4d528a024aa99383
Stella <amarachukwustella.nneji@my.jcu.edu.au>
2023-10-30 08:04:10 +1000

    Modified prac 6 guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -19,8 +19,8 @@ def main():
         print(f"{guitar} added.")
         name = input("Name: ")
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print("\nThese are my guitars:")
     for i, guitar in enumerate(guitars, 1):
         vintage_string = "(vintage)" if guitar.is_vintage() else ""
