c640060559920d26dd98d861c66e12ad35ef2405
Stella <amarachukwustella.nneji@my.jcu.edu.au>
2023-11-05 15:46:07 +1000

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -13,5 +13,18 @@ def main():
     for guitar in guitars:
         print(f"{guitar.name} ({guitar.year}) : {guitar.cost:,.2f}")
 
+    guitar_name = input("New guitar: ")
+    while guitar_name != "":
+        guitar_year = input("Year: ")
+        guitar_cost = float(input("Cost: $"))
+        guitar = Guitar(guitar_name, int(guitar_year), float(guitar_cost))
+        guitars.append(guitar)
+        guitars.sort()
+        guitar_name = input("New guitar: ")
+        out_file = open('guitars.csv', 'w')
+        for guitar in guitars:
+            out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+        out_file.close()
+
 
 main()
