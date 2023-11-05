de8e2807eba3d1a441a8b79b206989ce983494df
Stella <amarachukwustella.nneji@my.jcu.edu.au>
2023-11-05 19:03:02 +1000

    Add prac 07 project management load project function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,15 +3,20 @@ project_management.py
 Estimate - 5 hours
 Actual - hours
 """
+import csv
+
+from prac_07.project import ProjectManagement
+
 MENU = ("- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date"
         "\n- (A)dd new project\n- (U)pdate project\n- (Q)uit\n>>> ")
 
 
 def main():
     choice = input(MENU).upper()
+    file_name = "projects.txt "
     while choice != "Q":
         if choice == "L":
-            pass
+            load_project(file_name)
         elif choice == "S":
             pass
         elif choice == "D":
@@ -26,6 +31,16 @@ def main():
             print("Invalid choice")
         choice = input(MENU).upper()
     print("Thank you for using custom-built project management software.")
+
+
+def load_project(file_name):
+    projects = []
+    with open(file_name, "r", encoding="UTF-8") as in_file:
+        reader = csv.reader(in_file, delimiter='\t')
+        in_file.readline()
+        for row in reader:
+            projects.append(ProjectManagement(row[0], row[1], int(row[2]), float(row[3]), int(row[4])))
+    return projects
 
 
 main()
