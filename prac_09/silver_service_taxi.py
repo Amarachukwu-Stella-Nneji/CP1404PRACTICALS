f9ae28644a92493da799265c5b7176217867549e
Stella <amarachukwustella.nneji@my.jcu.edu.au>
2023-11-17 08:40:31 +1000

    Add prac 09 silver_service_taxi.py and silver_service_taxi_test.py

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,15 @@
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.price_per_km *= fanciness
+
+    def get_fare(self):
+        return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        return super().__str__() + f" plus a flagfall of ${self.flagfall}"
---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,17 @@
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    silver_taxi = SilverServiceTaxi("Silver Taxi", 100, 2)
+    silver_taxi.drive(18)
+    print(silver_taxi.get_fare())
+    silver_service_taxi = SilverServiceTaxi("Fancy Taxi", 100, 2.37)
+    silver_service_taxi.drive(70)
+    print(silver_service_taxi.get_fare())
+    silver_service_taxi.start_fare()
+    silver_service_taxi.drive(50)
+    print(silver_service_taxi.get_fare())
+    print(f"Taxi: {silver_service_taxi}\nCurrent Fare: ${silver_service_taxi.get_fare()}")
+
+
+main()
